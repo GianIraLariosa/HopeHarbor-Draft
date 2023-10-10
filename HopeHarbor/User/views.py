7014b42093594c5de506687b30d269e12af6759e
Gian Ira B. Lariosa <gianlariosa@gmail.com>
2023-10-10 09:25:35 +0800

    Added functionless login and registration pages

--- HopeHarbor/User/views.py
+++ HopeHarbor/User/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
 
 # Create your views here.
+
+def login(request):
+    template = loader.get_template('login.html')
+    return HttpResponse(template.render())
+
+
+def register(request):
+    template_register = loader.get_template('registration.html')
+    return HttpResponse(template_register.render())
